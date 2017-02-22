04c950a623a0b231e57ceb5ccfca510b93dedfd5
Haoran Shao <haoran@dsteam-dev-4.localdomain>
2017-02-22 22:43:14 +0000

    Haoran Shao revised for penn museum

--- models/CsvImport/ColumnMap.php
+++ models/CsvImport/ColumnMap.php
@@ -6,6 +6,7 @@
  * @copyright Copyright 2007-2012 Roy Rosenzweig Center for History and New Media
  * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
  * @package CsvImport
+ * Revised by Haoran Shao for adding NealineCoverage type
  */
 abstract class CsvImport_ColumnMap
 {
@@ -16,6 +17,7 @@ abstract class CsvImport_ColumnMap
     const TYPE_PUBLIC = 'Public';
     const TYPE_FEATURED = 'Featured';
     const TYPE_ITEM_TYPE = 'ItemType';
+    const TYPE_NEATLINE = 'NeatlineCoverage';
 
     protected $_columnName;
     protected $_type;
